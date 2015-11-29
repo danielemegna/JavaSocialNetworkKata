9f68b7071c8c260d4c5619b2e9616cf42ce6ce3c
Daniele Megna <megna.dany@gmail.com>
2015-11-29 13:37:20 +0100

    Developed input adapter for post commands

--- src/ConsoleInputAdapter.java
+++ src/ConsoleInputAdapter.java
@@ -1,4 +1,9 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ConsoleInputAdapter implements InputAdapter {
+
+    public static final String POST_MESSAGE_COMMAND_REGEX = "(.+) -> (.+)";
 
     private final ISocialNetwork socialNetwork;
 
@@ -7,6 +12,18 @@ public class ConsoleInputAdapter implements InputAdapter {
     }
 
     public void command(String command) {
+
+        Matcher matcher = Pattern
+            .compile(POST_MESSAGE_COMMAND_REGEX)
+            .matcher(command);
+
+        if(matcher.matches()) {
+            String username = matcher.group(1);
+            String message = matcher.group(2);
+            socialNetwork.post(username, message);
+            return;
+        }
+
         socialNetwork.reading(command);
     }
 }
--- test/ConsoleInputAdapterTest.java
+++ test/ConsoleInputAdapterTest.java
@@ -1,20 +1,38 @@
 import org.jmock.Expectations;
 import org.jmock.Mockery;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ConsoleInputAdapterTest {
 
+    private Mockery context;
+    private ISocialNetwork socialNetwork;
+    private ConsoleInputAdapter consoleInputAdapter;
+
+    @Before
+    public void setUp() {
+        context = new Mockery();
+        socialNetwork = context.mock(ISocialNetwork.class);
+        consoleInputAdapter = new ConsoleInputAdapter(socialNetwork);
+    }
+
     @Test
     public void passingUsername_willTriggerReadingCommand() {
-        Mockery context = new Mockery();
-        ISocialNetwork socialNetwork = context.mock(ISocialNetwork.class);
-        InputAdapter consoleInputAdapter = new ConsoleInputAdapter(socialNetwork);
-
         context.checking(new Expectations() {{
             oneOf(socialNetwork).reading("Username");
         }});
 
         consoleInputAdapter.command("Username");
+        context.assertIsSatisfied();
+    }
+
+    @Test
+    public void postAMessage() {
+        context.checking(new Expectations() {{
+            oneOf(socialNetwork).post("Username", "This is the message");
+        }});
+
+        consoleInputAdapter.command("Username -> This is the message");
         context.assertIsSatisfied();
     }
 }
